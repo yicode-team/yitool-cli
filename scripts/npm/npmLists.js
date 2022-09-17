e982a194aac3f672d52d8ccd8198b99f13787b01
chensuiyi <chenbimo@gmail.com>
2022-09-17 11:41:22 +0800

    【功能完善】: 优化显示格式

--- scripts/npm/npmLists.js
+++ scripts/npm/npmLists.js
@@ -1,31 +1,32 @@
+import { padStart as _padStart, padEnd as _padEnd } from 'lodash-es';
 export default [
     {
-        name: 'npm [ https://www.npmjs.org ]',
+        name: `${_padEnd('npm', 12)} https://www.npmjs.org`,
         home: 'https://www.npmjs.org',
         value: 'https://registry.npmjs.org'
     },
     {
-        name: 'taobao [ https://npmmirror.com ]',
+        name: `${_padEnd('taobao', 12)} https://npmmirror.com`,
         home: 'https://npmmirror.com',
         value: 'https://registry.npmmirror.com'
     },
     {
-        name: 'yarn [ https://yarnpkg.com ]',
+        name: `${_padEnd('yarn', 12)} https://yarnpkg.com`,
         home: 'https://yarnpkg.com',
         value: 'https://registry.yarnpkg.com'
     },
     {
-        name: 'cnpm [ https://cnpmjs.org ]',
+        name: `${_padEnd('cnpm', 12)} https://cnpmjs.org`,
         home: 'https://cnpmjs.org',
         value: 'https://r.cnpmjs.org'
     },
     {
-        name: 'tencent [ https://mirrors.cloud.tencent.com/npm ]',
+        name: `${_padEnd('tencent', 12)} https://mirrors.cloud.tencent.com/npm`,
         home: 'https://mirrors.cloud.tencent.com/npm/',
         value: 'https://mirrors.cloud.tencent.com/npm'
     },
     {
-        name: 'npmMirror [ https://skimdb.npmjs.com ]',
+        name: `${_padEnd('npmMirror', 12)} https://skimdb.npmjs.com`,
         home: 'https://skimdb.npmjs.com',
         value: 'https://skimdb.npmjs.com/registry'
     }
